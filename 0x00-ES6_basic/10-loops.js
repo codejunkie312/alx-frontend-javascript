577861fc7909afe4f77056e1992f340f131d5424
codejunkie312 <anas.sahimi1@gmail.com>
2024-04-09 22:51:16 +0000

    10. For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const tempArray = [];
+  for (const value of array) {
+    tempArray.push(appendString + value);
+  }
+
+  return tempArray;
+}
