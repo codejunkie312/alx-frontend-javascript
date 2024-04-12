0396978c5b92137dd0be78facf9abbe60d775c6f
codejunkie312 <anas.sahimi1@gmail.com>
2024-04-12 01:14:57 +0000

    10. Vroom

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+class car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
+
+export default car;
