2c8c678bc8e621d9df648d8a2b2465bc2fc584e8
codejunkie312 <anas.sahimi1@gmail.com>
2024-04-12 00:47:16 +0000

    7. Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,18 @@
+const util = require('util');
+
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+
+  [util.inspect.custom]() {
+    return `Airport [${this._code}] { _name: ${this._name}, _code: ${this._code} }`;
+  }
+}
+
+export default Airport;
