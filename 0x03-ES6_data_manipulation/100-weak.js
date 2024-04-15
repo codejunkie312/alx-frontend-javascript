531a6a4f581977ffa3dc8fb79d523cb7d2973298
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2024-04-15 19:36:15 +0100

    11. Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,7 @@
+export const weakMap = new WeakMap();
+export const queryAPI = (endpoint) => {
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1 || 1);
+};
